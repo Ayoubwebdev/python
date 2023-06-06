58270a17c12677118ee54143fe1235c2c85902dc
ayoubwebdev <ayobdu62@outlook.fr>
2023-06-06 12:03:31 +0200

    Cours sur les variables

--- variables.py
+++ variables.py
@@ -12,14 +12,14 @@ text2 = "foo bar baz"
 print (text2)
 
 # booléen, boolean
-python  is cool = True
-print (python is cool)
+python_is_cool = True
+print (python_is_cool)
 
-python is boring = false
-print(python is boring)
+python_is_boring = False
+print(python_is_boring)
 
 # valeur nulle, null value
-accepted terms = None
+user_accepted_terms = None
 print (None)
 
 #type de données
@@ -27,8 +27,8 @@ print( type(number1))
 print(type(number2))
 print(type(text1))
 print(type(text2))
-print(type(python_is_ cool))
-print(type(python_is_ boring))
+print(type(python_is_cool))
+print(type(python_is_boring))
 print(type(user_accepted_terms))
 
 #verification du type de données 
@@ -46,7 +46,7 @@ number3 = 0
 print (bool(number3 ))
 
 #transtypage str -> int
-print (type())
+print (type)
 
 #génère l'erreur : ValueError: Invalid literal for
 #print (type(int(text)))
@@ -62,3 +62,47 @@ print (type (int(text3)))
 # int () convertit vers integer
 # float() convertit vers float
 # bool () convertit vers boolean
+
+# chaine de caractéres, string
+# Cette notation permet d'utiliser des sauts de ligne
+text4 = """<div>
+ <h1>Titre de premier niveau</h1>
+ <div>
+ """
+print(text4)
+
+ # /n est équivalent à un saut de ligne
+ # /t est équivalent à une tabulation
+ text5="<div>/n/t<h1>Titre_de_premier_niveau</h1>/n</div>/n"
+
+ print(text5)
+
+ # la backslash seul est le caractère d'échappement
+ # \"est équivalent à une guillemet
+ # \\ est équivalent à un back slash \
+ text6 = "Foo \"Bar\"Baz"
+ text7 = "C:\\Program Files\\Foo"
+ print(text7)
+ 
+ # permutez les deux variables a et b en utilisant l'opérateur d'affectation et le nom des variables 
+ a = 123
+ b = 42
+
+ # permutation des valeurs à l'aide d'une variante temporaire
+ c = b
+ b = a 
+ a = c
+
+ print(a)
+ print(b)
+
+
+ # permutation des valeurs à l'aide d'opérations arithmétiques
+ a = a + b 
+ b = a - b
+ a = a - b 
+
+ print(a)
+ print(b)
+ 
+ 
